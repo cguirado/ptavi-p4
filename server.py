2f61d1782e4d0593e6d6531a7bf387b5ec1aa946
c.guirado <cgboluda@gmail.com>
2015-11-02 15:52:22 +0100

    8 y 9 ok

--- server.py
+++ server.py
@@ -6,13 +6,26 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
-
+import json
+import time
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
     dicserv ={}
+    def register2json(self):
+        #fich_json = json.dump(self.dicserv )
+        newfich = "registered.json"
+        with open(newfich, 'w') as ficherojson:
+            json.dump(self.dicserv, ficherojson)
+
+    def json2registered(self):
+        try:
+            with open("registered.json",'r') as existe:
+                self.dicserv= json.load(existe)
+        except:
+            pass
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         #self.wfile.write(b"Hemos recibido tu peticion")
@@ -20,7 +33,9 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         PORT = self.client_address[1]
         print("IP: ", IP)#Lo que tiene mi clnt con self.cl..
         print("Puerto: ",PORT )
-
+        if len(self.dicserv) == 0:
+            self.json2registered()
+        print(self.dicserv)
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             lineb = self.rfile.read()
@@ -31,15 +46,17 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             (metodo, direccion, elresto, expire, valor) = line.split()
             if metodo != "REGISTER" and not "@" in direccion:
                 break
+            formato = '%Y-%m-%d %H:%M:%S'
+            tiempo = time.strftime(formato, time.gmtime(int(valor)))
             if int(valor) == 0:
                 del self.dicserv[direccion]
-                self.wfile.write(b"SIP/2.0 200 OK")
-                print(self.dicserv)
+                #print(self.dicserv)
             else:
                 USER = direccion.split(":")[1]
-                self.dicserv[direccion] = [IP ,valor]
-                self.wfile.write(b"SIP/2.0 200 OK"+b"\r\n"+b"\r\n")
-                print (self.dicserv)
+                self.dicserv[direccion] = ["IP: " + str(IP),"EXPIRE: "+ tiempo]
+            self.wfile.write(b"SIP/2.0 200 OK"+b"\r\n"+b"\r\n")
+            self.register2json()
+            print ("AQUI",self.dicserv)
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     PORT = int(sys.argv[1])

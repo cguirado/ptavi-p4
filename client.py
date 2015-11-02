eaad633447b9eb07bf2a131ba61d8aee7af10411
c.guirado <cgboluda@gmail.com>
2015-11-02 10:43:47 +0100

    7 ok

--- client.py
+++ client.py
@@ -14,19 +14,21 @@ SERVER = 'localhost'
 IP = sys.argv[1]
 PORT = int(sys.argv[2])
 
-
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 # Contenido que vamos a enviar
+if not len(sys.argv) != 5:
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 if sys.argv[3] == "register":
-    USER = " ".join(sys.argv[4:])
+    USER = " ".join(sys.argv[4:-1])
 else:
     sys.exit("Necesito un register")
 
-LINE = ("REGISTER "+"sip:"+USER+" SIP/2.0"+ "\r\n"+"\r\n")
+Expire = sys.argv[5]
+LINE = ("REGISTER "+"sip:"+USER+" SIP/2.0"+ "\r\n"+"Expire:"+" "+Expire+"\r\n"+"\r\n")
 print("Enviando:", LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
